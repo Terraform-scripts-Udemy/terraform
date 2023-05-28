1071343ddeaa110fc1af938b5c8694a5f5d45184
MallegowdaKM <mallegowdakm2906@gmail.com>
2023-05-28 22:54:01 +0530

    updated with security_groups

---
+++ security_groups.tf
@@ -0,0 +1,55 @@
+# Create Security Group - SSH Traffic
+resource "aws_security_group" "ec2_ssh" {
+  name        = "ec2_ssh"
+  description = "enableing the SSH port"
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all ip and ports outbound"    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2_ssh"
+  }
+}
+
+# Create Security Group - Web Traffic
+resource "aws_security_group" "tomcat_port" {
+  name        = "tomcat_port"
+  description = "enabling tomcat port"
+  ingress {
+    description = "Allow Port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+  egress {
+    description = "Allow all ip and ports outbound"    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vpc-web"
+  }
+}
