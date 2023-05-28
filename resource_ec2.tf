20560609a60a5dd04ba52b439c07f82ee59c3111
MallegowdaKM <mallegowdakm2906@gmail.com>
2023-05-28 22:44:48 +0530

    updated_with_variables

--- resource_ec2.tf
+++ resource_ec2.tf
@@ -1,8 +1,8 @@
 #EC2 instance
 resource "aws_instance" "ec2instance" {
     ami = ami-008b85aa3ff5c1b02
-    instance_type = t2.micro
-    key_name = practisekey
+    instance_type = var.instance_type
+    key_name = var.instance_keypair
     tags = {
         "Name" = "ec2_demo"
     }
