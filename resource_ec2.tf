2a488b8dc45d5cbdbff02cdb7712bb52faaba6a7
MallegowdaKM <mallegowdakm2906@gmail.com>
2023-05-28 23:27:54 +0530

    updated_with_data_source

--- resource_ec2.tf
+++ resource_ec2.tf
@@ -1,6 +1,6 @@
 #EC2 instance
 resource "aws_instance" "ec2instance" {
-    ami = ami-008b85aa3ff5c1b02
+    ami = data.aws_ami.amazonlinuxos
     instance_type = var.instance_type
     key_name = var.instance_keypair
     tags = {
